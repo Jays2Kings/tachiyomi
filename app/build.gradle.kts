DaviDeMeo/tachiyomiJ2K

Targeting Android-S

app/build.gradle.kts
@@ -28,11 +28,11 @@ fun runCommand(command: String): String {
 }
 
 android {
-    compileSdkVersion(AndroidVersions.compileSdk)
+    compileSdkPreview = AndroidVersions.compileSdk
 
     defaultConfig {
-        minSdkVersion(AndroidVersions.minSdk)
-        targetSdkVersion(AndroidVersions.targetSdk)
+        minSdk = AndroidVersions.minSdk
+        targetSdkPreview = AndroidVersions.targetSdk
         applicationId = "eu.kanade.tachiyomi"
         versionCode = AndroidVersions.versionCode
         versionName = AndroidVersions.versionName
@@ -95,7 +95,7 @@ dependencies {
     implementation("tachiyomi.sourceapi:source-api:1.1")
 
     // Android X libraries
-    implementation("androidx.appcompat:appcompat:1.3.0-rc01")
+    implementation("androidx.appcompat:appcompat:1.4.0-alpha01")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.android.material:material:1.4.0-beta01")
     implementation("androidx.recyclerview:recyclerview:1.2.0")
@@ -176,7 +176,7 @@ dependencies {
     implementation("org.jsoup:jsoup:1.13.1")
 
     // Job scheduling
-    implementation("androidx.work:work-runtime-ktx:2.5.0")
+    implementation("androidx.work:work-runtime-ktx:2.7.0-alpha03")
 
     implementation("com.google.android.gms:play-services-gcm:17.0.0")
 

Jays2Kings/tachiyomi

app/build.gradle.kts
@@ -28,11 +28,11 @@ fun runCommand(command: String): String {
 }
 
 android {
-    compileSdkVersion(AndroidVersions.compileSdk)
+    compileSdkPreview = AndroidVersions.compileSdk
 
     defaultConfig {
-        minSdkVersion(AndroidVersions.minSdk)
-        targetSdkVersion(AndroidVersions.targetSdk)
+        minSdk = AndroidVersions.minSdk
+        targetSdkPreview = AndroidVersions.targetSdk
         applicationId = "eu.kanade.tachiyomi"
         versionCode = AndroidVersions.versionCode
         versionName = AndroidVersions.versionName
@@ -98,7 +98,7 @@ dependencies {
     implementation("tachiyomi.sourceapi:source-api:1.1")
 
     // Android X libraries
-    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.4.0-alpha03")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.recyclerview:recyclerview:1.2.1")
@@ -180,7 +180,7 @@ dependencies {
     implementation("org.jsoup:jsoup:1.13.1")
 
     // Job scheduling
-    implementation("androidx.work:work-runtime-ktx:2.5.0")
+    implementation("androidx.work:work-runtime-ktx:2.7.0-alpha03")
 
     implementation("com.google.android.gms:play-services-gcm:17.0.0")
 